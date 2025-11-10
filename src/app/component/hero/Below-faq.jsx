f30b4a159beb1c34226c8fc3191926fb7814b50e
waince654 <mfarooqjjj5@gmail.com>
2025-11-10 00:09:22 -0800

    below-faq section

---
+++ src/app/component/hero/Below-faq.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+import { PiGreaterThanBold } from 'react-icons/pi'
+
+const Belowfaq = () => {
+  return (
+    <div>
+       <div className="relative mt-5 flex justify-center items-center gap-2">
+        <h4 className="text-blue-900 mt-6 font-sans-serif-16px text-xl sm:text-2xl md:text-3xl lg:text-[35px] font-bold ">
+                   Do Something for Your Privacy
+        </h4>
+        </div>
+        <div className="relative mt-4 flex justify-center items-center gap-2">
+        <p className="text-center text-blue-600  mt-13 max-w-2xl mx-auto text-xl sm:text-2xl md:text-3xl  ">Be Smart, Be Anonymous!</p>
+        </div>
+        {/* mix my crpto  button */}
+          <div className="flex justify-center mt-8 items-center min-h-[18vh]">
+        <Link href="/#">  <button className=" flex lg:px-25 py-1 lg:py-3 gap-9 text-lg font-semibold text-white bg-cyan-300 rounded-4xl hover:-translate-y-2 transition-all duration-300 shadow-md active:scale-95 sm:px-20 sm:py-4">
+         
+          Mix My Crypto  <span className="mt-2"> <PiGreaterThanBold /></span>
+        </button></Link>
+       
+      </div>
+    </div>
+  )
+}
+
+export default Belowfaq;
+
--- src/app/component/hero/Faq.jsx
+++ src/app/component/hero/Faq.jsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import { PiGreaterThanBold } from "react-icons/pi";
 import { Link as LinkIcon } from "lucide-react";
+import Belowfaq from "./Below-faq";
 
 
 const faqData = [
@@ -76,7 +76,7 @@ export default function FaqSection() {
         </div>
       </div>
 
-      {/* ✅ Grid Layout */}
+      {/*  Grid Layout */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {faqData.map((faq, index) => (
           <div
@@ -137,22 +137,7 @@ export default function FaqSection() {
         </button>
        
       </div>
-       <div className="relative mt-5 flex justify-center items-center gap-2">
-        <h4 className="text-blue-900 mt-6 font-sans-serif-16px text-xl sm:text-2xl md:text-3xl lg:text-[35px] font-bold ">
-                   Do Something for Your Privacy
-        </h4>
-        </div>
-        <div className="relative mt-4 flex justify-center items-center gap-2">
-        <p className="text-center text-blue-600  mt-13 max-w-2xl mx-auto text-xl sm:text-2xl md:text-3xl  ">Be Smart, Be Anonymous!</p>
-        </div>
-        {/* mix my crpto  button */}
-          <div className="flex justify-center mt-8 items-center min-h-[18vh]">
-        <Link href="/#">  <button className=" flex px-25 py-1 lg:py-3 gap-9 text-lg font-semibold text-white bg-cyan-300 rounded-4xl hover:-translate-y-2 transition-all duration-300 shadow-md active:scale-95 sm:px-20 sm:py-4">
-         
-          Mix My Crypto  <span className="mt-2"> <PiGreaterThanBold /></span>
-        </button></Link>
-       
-      </div>
+      <Belowfaq />
     </section>
   );
 }
